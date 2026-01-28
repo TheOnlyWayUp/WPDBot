8ac480cae61e93b8f649a585f3cd85f776370565
Dhanush R <76237496+TheOnlyWayUp@users.noreply.github.com>
2026-01-28 18:59:53 +0530

    fix: Enable member intent

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ import aiohttp
 
 intents = disnake.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or("!"),
